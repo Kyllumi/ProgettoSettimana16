6f84d7f96c4654adc3c53678738a2493e59acda4
Kleo <kleoruda9@gmail.com>
2024-03-06 16:40:35 +0100

    bb

--- index.php
+++ index.php
@@ -12,45 +12,52 @@ $conn = $PDOConn->getConnection(); //Mi connetto
 
 $userDTO = new UserDTO($conn);
 
-if (isset($_REQUEST['firstname'])) {
-    $firstname = $_REQUEST['firstname'];
-    $lastname = $_REQUEST['lastname'];
-    $email = $_REQUEST['email'];
-    $password = $_REQUEST['password'];
-    $admin = $_REQUEST['admin'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['firstname'])) {
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $admin = $_POST['admin'];
 
-    $res = $userDTO->saveUser([
-        'firstname' => $firstname,
-        'lastname' => $lastname,
-        'email' => $email,
-        'password' => $password,
-        'admin' => $admin
-    ]);
+        $res = $userDTO->saveUser([
+            'firstname' => $firstname,
+            'lastname' => $lastname,
+            'email' => $email,
+            'password' => $password,
+            'admin' => $admin
+        ]);
+    }
+
+    if (isset($_REQUEST['id']) && $_REQUEST['action'] == 'update') {
+        $id = intval($_REQUEST['id']);
+        // Recupera i dati dell'utente dalla richiesta POST
+        $firstname = $_POST['firstnameUp'];
+        $lastname = $_POST['lastnameUp'];
+        $email = $_POST['emailUp'];
+        $password = $_POST['passwordUp'];
+        $admin = $_POST['adminUp'];
+
+        echo $firstname . ' ' . $lastname . ' ' . $email . ' ' . $password . ' ' . $admin;
+
+        $res = $userDTO->updateUser([
+            'id' => $id,
+            'firstname' => $firstname,
+            'lastname' => $lastname,
+            'email' => $email,
+            'password' => $password,
+            'admin' => $admin
+        ]);
+
+        // Redirect alla pagina principale dopo l'aggiornamento
+        // header('Location: index.php');
+        // exit;
+    }
+
 }
 
-if (isset($_REQUEST['id']) && $_REQUEST['action'] == 'edit') {
-    $firstname = $_REQUEST['firstname'];
-    $lastname = $_REQUEST['lastname'];
-    $email = $_REQUEST['email'];
-    $password = $_REQUEST['password'];
-    $admin = $_REQUEST['admin'];
-    $id = intval($_REQUEST['id']);
-
-    echo $id;
-    var_dump($firstname);
-
-    // $res = $userDTO->updateUser([
-    //     'id' => $id,
-    //     'firstname' => $firstname,
-    //     'lastname' => $lastname,
-    //     'email' => $email,
-    //     'password' => $password,
-    //     'admin' => $admin
-    // ]);
-}
-
-if (isset($_REQUEST['id']) && $_REQUEST['action'] == 'delete') {
-    $id = intval($_REQUEST['id']);
+if (isset($_GET['id']) && $_GET['action'] == 'delete') {
+    $id = intval($_GET['id']);
 
     $res = $userDTO->deleteUser($id);
 
@@ -84,14 +91,14 @@ $res = $userDTO->getAll();
         </div>
     </nav>
     <h1 class="text-center my-4">Pannello di amministrazione</h1>
-    <div class="d-flex justify-content-center my-4">
-
-        <a href="create.php" class="btn btn-success w-25" data-bs-toggle="modal" data-bs-target="#creaUtente">
-            Aggiungi utenti
-        </a>
-
-    </div>
     <div class="container">
+        <!-- Form per aggiungere nuovi utenti -->
+        <div class="d-flex justify-content-center my-4">
+            <a href="#" class="btn btn-success w-25" data-bs-toggle="modal" data-bs-target="#creaUtente">
+                Aggiungi utenti
+            </a>
+        </div>
+        <!-- Tabella per i record esistenti -->
         <table class="table table-striped table-hover">
             <thead>
                 <tr>
@@ -105,10 +112,10 @@ $res = $userDTO->getAll();
                 </tr>
             </thead>
             <tbody>
-
                 <?php
                 if ($res) {
                     foreach ($res as $record) {
+                        $modalId = "modificaUtente_" . $record["id"]; // Genera un ID univoco per ogni modale
                         ?>
                         <tr>
                             <td>
@@ -126,170 +133,121 @@ $res = $userDTO->getAll();
                             <td>
                                 <?= $record["password"] ?>
                             </td>
-                            <td class="text-center align-middle">
-                                <?php if ($record["admin"] == 0): ?>
-                                    <i class="fas fa-circle text-danger"></i>
-                                <?php else: ?>
-                                    <i class="fas fa-circle text-success"></i>
-                                <?php endif; ?>
+                            <td>
+                                <?= $record["admin"] ?>
                             </td>
                             <td>
-                                <a href="index.php?action=edit&id=<?= $record["id"] ?>" class="btn btn-primary"
-                                    data-bs-toggle="modal" data-bs-target="#modificaUtente">Modifica</a>
+                                <a href="#" class="btn btn-primary" data-bs-toggle="modal"
+                                    data-bs-target="#<?= $modalId ?>">Modifica</a>
                                 <a href="index.php?action=delete&id=<?= $record["id"] ?>" class="btn btn-danger">Elimina</a>
                             </td>
                         </tr>
+
+                        <!-- Modale per la modifica -->
+                        <div class="modal fade" id="<?= $modalId ?>" tabindex="-1" aria-labelledby="<?= $modalId ?>Label"
+                            aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="modificaUtente Label">Gestione Utenti</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <form method="post" action="index.php">
+                                            <input name="id" type="hidden" class="form-control" id="id" aria-describedby="id"
+                                                value="<?= $record["id"] ?>">
+                                            <div class="mb-3">
+                                                <label for="firstname" class="form-label">Nome</label>
+                                                <input name="firstnameUp" type="text" class="form-control" id="firstname"
+                                                    aria-describedby="firstname" value="<?= $record["firstname"] ?>">
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="lastname" class="form-label">Cognome</label>
+                                                <input name="lastnameUp" type="text" class="form-control" id="lastname"
+                                                    aria-describedby="lastname" value="<?= $record["lastname"] ?>">
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="email" class="form-label">Email</label>
+                                                <input name="emailUp" type="email" class="form-control" id="email"
+                                                    aria-describedby="emailHelp" value="<?= $record["email"] ?>">
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="password" class="form-label">Password</label>
+                                                <input name="passwordUp" type="password" class="form-control" id="password"
+                                                    value="<?= $record["password"] ?>">
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="admin" class="form-label">Admin</label>
+                                                <input name="adminUp" type="number" class="form-control" id="admin"
+                                                    aria-describedby="admin" min="0" max="1" value="<?= $record["admin"] ?>">
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary"
+                                                    data-bs-dismiss="modal">Chiudi</button>
+                                                <button name="action" value="update" type="submit"
+                                                    class="btn btn-primary">Modifica</button>
+                                            </div>
+                                        </form>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                         <?php
                     }
                 }
-
                 ?>
-
             </tbody>
         </table>
     </div>
 
-
+    <!-- Modale nuovi utenti -->
+    <div class="modal fade" id="creaUtente" tabindex="-1" aria-labelledby="creaUtenteLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="creaUtenteLabel">Gestione Utenti</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form method="post" action="index.php">
+                        <div class="mb-3">
+                            <label for="firstname" class="form-label">Nome</label>
+                            <input name="firstname" type="text" class="form-control" id="firstname"
+                                aria-describedby="firstname">
+                        </div>
+                        <div class="mb-3">
+                            <label for="lastname" class="form-label">Cognome</label>
+                            <input name="lastname" type="text" class="form-control" id="lastname"
+                                aria-describedby="lastname">
+                        </div>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email</label>
+                            <input name="email" type="email" class="form-control" id="email"
+                                aria-describedby="emailHelp">
+                        </div>
+                        <div class="mb-3">
+                            <label for="password" class="form-label">Password</label>
+                            <input name="password" type="password" class="form-control" id="password">
+                        </div>
+                        <div class="mb-3">
+                            <label for="admin" class="form-label">Admin</label>
+                            <input name="admin" type="number" class="form-control" id="admin" aria-describedby="admin"
+                                min="0" max="1">
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
+                            <button type="submit" class="btn btn-primary">Crea</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
 </body>
 
 </html>
-
-
-
-<!-- Modali -->
-<div class="modal fade" id="creaUtente" tabindex="-1" aria-labelledby="creaUtenteLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title fs-5" id="creaUtente
-    Label">Gestione Utenti</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <form method="post" action="index.php">
-                    <div class="mb-3">
-                        <label for="firstname" class="form-label">Nome</label>
-                        <input name="firstname" type="text" class="form-control" id="firstname"
-                            aria-describedby="firstname">
-                    </div>
-                    <div class="mb-3">
-                        <label for="lastname" class="form-label">Cognome</label>
-                        <input name="lastname" type="text" class="form-control" id="lastname"
-                            aria-describedby="lastname">
-                    </div>
-                    <div class="mb-3">
-                        <label for="email" class="form-label">Email</label>
-                        <input name="email" type="email" class="form-control" id="email" aria-describedby="emailHelp">
-                    </div>
-                    <div class="mb-3">
-                        <label for="password" class="form-label">Password</label>
-                        <input name="password" type="password" class="form-control" id="password">
-                    </div>
-                    <div class="mb-3">
-                        <label for="admin" class="form-label">Admin</label>
-                        <input name="admin" type="number" class="form-control" id="admin" aria-describedby="admin"
-                            min="0" max="1">
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
-                        <button type="submit" class="btn btn-primary">Crea</button>
-                    </div>
-                </form>
-            </div>
-
-        </div>
-    </div>
-</div>
-
-<div class="modal fade" id="modificaUtente" tabindex="-1" aria-labelledby="modificaUtenteLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title fs-5" id="modificaUtente
-    Label">Gestione Utenti</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <form method="post" action="index.php">
-                    <div class="mb-3">
-                        <label for="firstname" class="form-label">Nome</label>
-                        <input name="firstname" type="text" class="form-control" id="firstname"
-                            aria-describedby="firstname">
-                    </div>
-                    <div class="mb-3">
-                        <label for="lastname" class="form-label">Cognome</label>
-                        <input name="lastname" type="text" class="form-control" id="lastname"
-                            aria-describedby="lastname">
-                    </div>
-                    <div class="mb-3">
-                        <label for="email" class="form-label">Email</label>
-                        <input name="email" type="email" class="form-control" id="email" aria-describedby="emailHelp">
-                    </div>
-                    <div class="mb-3">
-                        <label for="password" class="form-label">Password</label>
-                        <input name="password" type="password" class="form-control" id="password">
-                    </div>
-                    <div class="mb-3">
-                        <label for="admin" class="form-label">Admin</label>
-                        <input name="admin" type="number" class="form-control" id="admin" aria-describedby="admin"
-                            min="0" max="1">
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
-                        <a href="index.php?action=edit&id=<?= $record["id"] ?>" type="submit"
-                            class="btn btn-primary">Modifica</a>
-                    </div>
-                </form>
-            </div>
-
-        </div>
-    </div>
-</div>
-
-<!-- Modale -->
-<div class="modal fade" id="modificaUtente" tabindex="-1" aria-labelledby="modificaUtenteLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title fs-5" id="modificaUtente
-    Label">Gestione Utenti</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <form method="post" action="index.php">
-                    <div class="mb-3">
-                        <label for="firstname" class="form-label">Nome</label>
-                        <input name="firstname" type="text" class="form-control" id="firstname"
-                            aria-describedby="firstname">
-                    </div>
-                    <div class="mb-3">
-                        <label for="lastname" class="form-label">Cognome</label>
-                        <input name="lastname" type="text" class="form-control" id="lastname"
-                            aria-describedby="lastname">
-                    </div>
-                    <div class="mb-3">
-                        <label for="email" class="form-label">Email</label>
-                        <input name="email" type="email" class="form-control" id="email" aria-describedby="emailHelp">
-                    </div>
-                    <div class="mb-3">
-                        <label for="password" class="form-label">Password</label>
-                        <input name="password" type="password" class="form-control" id="password">
-                    </div>
-                    <div class="mb-3">
-                        <label for="admin" class="form-label">Admin</label>
-                        <input name="admin" type="number" class="form-control" id="admin" aria-describedby="admin"
-                            min="0" max="1">
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
-                        <a href="index.php?action=edit&id=<?= $record["id"] ?>" type="submit"
-                            class="btn btn-primary">Modifica</a>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
--- user.php
+++ user.php
@@ -34,19 +34,38 @@ class UserDTO
     }
     public function saveUser(array $user)
     {
-        $sql = "INSERT INTO users (firstname, lastname, email, password, admin) VALUES (:firstname, :lastname, :email, :password, :admin)";
-        $stm = $this->conn->prepare($sql);
-        $stm->execute(['firstname' => $user['firstname'], 'lastname' => $user['lastname'], 'email' => $user['email'], 'password' => $user['password'], 'admin' => $user['admin']]);
-        return $stm->rowCount();
+        try {
+            $sql = "INSERT INTO users (firstname, lastname, email, password, admin) VALUES (:firstname, :lastname, :email, :password, :admin)";
+            $stm = $this->conn->prepare($sql);
+            $stm->execute(['firstname' => $user['firstname'], 'lastname' => $user['lastname'], 'email' => $user['email'], 'password' => $user['password'], 'admin' => $user['admin']]);
+            return $stm->rowCount();
+        } catch (PDOException $e) {
+            if ($e->errorInfo[1] == 1062) {
+                echo "Email address already exists!";
+            } else {
+                echo "An error occurred: " . $e->getMessage();
+            }
+        }
     }
     public function updateUser(array $user)
     {
-        var_dump($user);
-        $sql = "UPDATE users SET firstname = :firstname, lastname = :lastname, email = :email, password = :password, admin = :admin WHERE id = :id";
-        $stm = $this->conn->prepare($sql);
-        $stm->execute(['firstname' => $user['firstname'], 'lastname' => $user['lastname'], 'email' => $user['email'], 'password' => $user['password'], 'admin' => $user['admin']]);
-        return $stm->rowCount();
+        try {
+            $sql = "UPDATE users SET firstname = :firstname, lastname = :lastname, email = :email, password = :password, admin = :admin WHERE id = :id";
+            $stm = $this->conn->prepare($sql);
+            $stm->execute([
+                'id' => $user['id'],
+                'firstname' => $user['firstname'],
+                'lastname' => $user['lastname'],
+                'email' => $user['email'],
+                'password' => $user['password'],
+                'admin' => $user['admin']
+            ]);
+            return $stm->rowCount();
+        } catch (PDOException $e) {
+            echo "An error occurred: " . $e->getMessage();
+        }
     }
+    
     public function deleteUser(int $id)
     {
         var_dump($id);
