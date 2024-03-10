154101b7e0129074f32c9c9feea3459e5f3bf239
Kleo <kleoruda9@gmail.com>
2024-03-10 10:39:40 +0100

    layout

--- index.php
+++ index.php
@@ -82,39 +82,45 @@ if (isset($_SESSION['userLogin'])) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Progetto Settimana 16</title>
+    <link rel="icon" href="img/klog-logo.png" type="image/png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 
-<body>
-    <nav class="navbar bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand">PS16</a>
+<body class="bg-dark p-0">
+    <nav class="navbar mt-0">
+        <div class="container-fluid justify-content-between align-items-center px-5">
+            <a class="navbar-brand">
+                <img src="img/klog-logo.png" alt="logo" style="width: 3rem; height: auto">
+            </a>
             <form class="d-flex" role="search">
-                <a href="logout.php" class="btn btn-outline-primary" type="submit">Logout</a>
+                <a href="logout.php" class="btn btn-outline-light mt-3" type="submit">Logout</a>
             </form>
         </div>
     </nav>
-    <h1 class="text-center my-4">Pannello di amministrazione</h1>
+    <h1 class="text-center text-light my-4 pb-3" style="border-bottom: 1px solid white; width: 50%; margin: 0 auto">
+        Pannello di amministrazione
+    </h1>
     <div class="container">
-        <!-- Form per aggiungere nuovi utenti -->
         <div class="d-flex justify-content-center my-4">
-            <a href="#" class="btn btn-success w-25" data-bs-toggle="modal" data-bs-target="#creaUtente">
+            <a href="#" class="btn btn-primary w-25" data-bs-toggle="modal" data-bs-target="#creaUtente">
                 Aggiungi utenti
             </a>
+
         </div>
         <!-- Tabella per i record esistenti -->
-        <table class="table table-striped table-hover">
+        <table class="table table-hover">
             <thead>
                 <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Nome</th>
-                    <th scope="col">Cognome</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Password</th>
-                    <th scope="col">Admin</th>
-                    <th scope="col">Azioni</th>
+                    <th scope="col" style="background-color: #0D6EFD60 !important; color: white;">ID</th>
+                    <th scope="col" style="background-color: #0D6EFD60 !important; color: white;">Nome</th>
+                    <th scope="col" style="background-color: #0D6EFD60 !important; color: white;">Cognome</th>
+                    <th scope="col" style="background-color: #0D6EFD60 !important; color: white;">Email</th>
+                    <th scope="col" style="background-color: #0D6EFD60 !important; color: white;">Password</th>
+                    <th scope="col" style="background-color: #0D6EFD60 !important; color: white;">Admin</th>
+                    <th scope="col" style="background-color: #0D6EFD60 !important; color: white;">Azioni</th>
                 </tr>
             </thead>
             <tbody>
@@ -124,34 +130,39 @@ if (isset($_SESSION['userLogin'])) {
                         $modalId = "modificaUtente_" . $record["id"]; // Genera un ID univoco per ogni modale
                         ?>
                         <tr>
-                            <td>
+                            <td style="background-color: #0D6EFD10 !important; color: white;">
                                 <?= $record["id"] ?>
                             </td>
-                            <td>
+                            <td style="background-color: #0D6EFD10 !important; color: white;">
                                 <?= $record["firstname"] ?>
                             </td>
-                            <td>
+                            <td style="background-color: #0D6EFD10 !important; color: white;">
                                 <?= $record["lastname"] ?>
                             </td>
-                            <td>
+                            <td style="background-color: #0D6EFD10 !important; color: white;">
                                 <?= $record["email"] ?>
                             </td>
-                            <td>
+                            <td style="background-color: #0D6EFD10 !important; color: white;">
                                 <?= $record["password"] ?>
                             </td>
-                            <td>
+                            <td style="background-color: #0D6EFD10 !important; color: white;">
                                 <?php
                                 if ($record["admin"] == 0) {
-                                    echo "No";
+                                    echo "";
                                 } else {
-                                    echo "Si";
+                                    echo '<i class="fas fa-check"></i>';
                                 }
                                 ?>
                             </td>
-                            <td>
-                                <a href="#" class="btn btn-primary" data-bs-toggle="modal"
-                                    data-bs-target="#<?= $modalId ?>">Modifica</a>
-                                <a href="index.php?action=delete&id=<?= $record["id"] ?>" class="btn btn-danger">Elimina</a>
+
+                            <td style="background-color: #0D6EFD10 !important; color: white;">
+                                <a href="#" class="btn btn-outline-primary me-2" data-bs-toggle="modal"
+                                    data-bs-target="#<?= $modalId ?>">
+                                    <i class="fas fa-user-edit"></i>
+                                </a>
+                                <a href="index.php?action=delete&id=<?= $record["id"] ?>" class="btn btn-outline-danger">
+                                    <i class="far fa-trash-alt"></i>
+                                </a>
                             </td>
 
                         </tr>
@@ -161,43 +172,42 @@ if (isset($_SESSION['userLogin'])) {
                         <div class="modal fade" id="<?= $modalId ?>" tabindex="-1" aria-labelledby="<?= $modalId ?>Label"
                             aria-hidden="true">
                             <div class="modal-dialog">
-                                <div class="modal-content">
+                                <div class="modal-content"
+                                style="background-color: #0D6EFD10; backdrop-filter: blur(10px);">
                                     <div class="modal-header">
-                                        <h1 class="modal-title fs-5" id="modificaUtente Label">Gestione Utenti</h1>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                            aria-label="Close"></button>
+                                        <h1 class="modal-title fs-5 text-light" id="modificaUtente Label">Gestione Utenti</h1>
                                     </div>
                                     <div class="modal-body">
                                         <form method="post" action="index.php">
                                             <input name="id" type="hidden" class="form-control" id="id" aria-describedby="id"
                                                 value="<?= $record["id"] ?>">
                                             <div class="mb-3">
-                                                <label for="firstname" class="form-label">Nome</label>
+                                                <label for="firstname" class="form-label text-light">Nome</label>
                                                 <input name="firstnameUp" type="text" class="form-control" id="firstname"
                                                     aria-describedby="firstname" value="<?= $record["firstname"] ?>">
                                             </div>
                                             <div class="mb-3">
-                                                <label for="lastname" class="form-label">Cognome</label>
+                                                <label for="lastname" class="form-label text-light">Cognome</label>
                                                 <input name="lastnameUp" type="text" class="form-control" id="lastname"
                                                     aria-describedby="lastname" value="<?= $record["lastname"] ?>">
                                             </div>
                                             <div class="mb-3">
-                                                <label for="email" class="form-label">Email</label>
+                                                <label for="email" class="form-label text-light">Email</label>
                                                 <input name="emailUp" type="email" class="form-control" id="email"
                                                     aria-describedby="emailHelp" value="<?= $record["email"] ?>">
                                             </div>
                                             <div class="mb-3">
-                                                <label for="password" class="form-label">Password</label>
+                                                <label for="password" class="form-label text-light">Password</label>
                                                 <input name="passwordUp" type="password" class="form-control" id="password"
                                                     value="<?= $record["password"] ?>">
                                             </div>
                                             <div class="mb-3">
-                                                <label for="admin" class="form-label">Admin</label>
+                                                <label for="admin" class="form-label text-light">Admin</label>
                                                 <input name="adminUp" type="number" class="form-control" id="admin"
                                                     aria-describedby="admin" min="0" max="1" value="<?= $record["admin"] ?>">
                                             </div>
                                             <div class="modal-footer">
-                                                <button type="button" class="btn btn-secondary"
+                                                <button type="button" class="btn btn-outline-secondary"
                                                     data-bs-dismiss="modal">Chiudi</button>
                                                 <button name="action" value="update" type="submit"
                                                     class="btn btn-primary">Modifica</button>
@@ -219,39 +229,39 @@ if (isset($_SESSION['userLogin'])) {
     <!-- Modale nuovi utenti -->
     <div class="modal fade" id="creaUtente" tabindex="-1" aria-labelledby="creaUtenteLabel" aria-hidden="true">
         <div class="modal-dialog">
-            <div class="modal-content">
+            <div class="modal-content"
+                style="background-color: #0D6EFD10; backdrop-filter: blur(10px);">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="creaUtenteLabel">Gestione Utenti</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <h1 class="modal-title fs-5 text-light" id="creaUtenteLabel">Gestione Utenti</h1>
                 </div>
                 <div class="modal-body">
                     <form method="post" action="index.php">
                         <div class="mb-3">
-                            <label for="firstname" class="form-label">Nome</label>
+                            <label for="firstname" class="form-label text-light">Nome</label>
                             <input name="firstname" type="text" class="form-control" id="firstname"
                                 aria-describedby="firstname">
                         </div>
                         <div class="mb-3">
-                            <label for="lastname" class="form-label">Cognome</label>
+                            <label for="lastname" class="form-label text-light">Cognome</label>
                             <input name="lastname" type="text" class="form-control" id="lastname"
                                 aria-describedby="lastname">
                         </div>
                         <div class="mb-3">
-                            <label for="email" class="form-label">Email</label>
+                            <label for="email" class="form-label text-light">Email</label>
                             <input name="email" type="email" class="form-control" id="email"
                                 aria-describedby="emailHelp">
                         </div>
                         <div class="mb-3">
-                            <label for="password" class="form-label">Password</label>
+                            <label for="password" class="form-label text-light">Password</label>
                             <input name="password" type="password" class="form-control" id="password">
                         </div>
                         <div class="mb-3">
-                            <label for="admin" class="form-label">Admin</label>
+                            <label for="admin" class="form-label text-light">Admin</label>
                             <input name="admin" type="number" class="form-control" id="admin" aria-describedby="admin"
                                 min="0" max="1">
                         </div>
                         <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
+                            <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Chiudi</button>
                             <button type="submit" class="btn btn-primary">Crea</button>
                         </div>
                     </form>
--- login.php
+++ login.php
@@ -1,3 +1,4 @@
+
 <?php
 session_start();
 ?>
@@ -14,7 +15,7 @@ session_start();
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors" />
     <meta name="generator" content="Hugo 0.122.0" />
     <title>Progetto Settimana 16</title>
-
+    <link rel="icon" href="img/klog-logo.png" type="image/png">
     <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/sign-in/" />
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3" />
@@ -28,20 +29,40 @@ session_start();
     <link href="https://getbootstrap.com/docs/5.3/examples/sign-in/sign-in.css" rel="stylesheet" />
 </head>
 
-<body class="d-flex align-items-center py-4 bg-body-tertiary">
-    <main class="form-signin w-100 m-auto">
+<body class="d-flex align-items-center py-4 bg-dark">
+    <main class="form-signin w-100 m-auto pt-0" style="border-radius: 2rem;
+            background-color: #0D6EFD30;
+            backdrop-filter: blur(5px);">
         <form action="controller.php" method="post">
-            <h1 class="h3 mb-3 fw-normal text-center">LOGIN</h1>
+            <div class="d-flex flex-column align-items-center justify-content-center my-2"
+            style="width: 100%; margin: auto;">
+                <div class="pb-2 mt-1">
+                    <img src="img/klog-logo.png" alt="logo" style="width: 6.5rem; height: auto">
+                </div>
+                <div class="pt-2"
+                style="border-top: solid 1px #0D6EFD; width: 100%;">
+                   <h1 class="h4 mb-3 fw-normal text-center text-light">Login</h1> 
+                </div>
+            </div>
+
 
             <div class="form-floating">
                 <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" name="email"
-                    value="" />
-                <label for="floatingInput">Email</label>
+                    value="" style="
+                background-color: #FFFFFF;
+                border-color: #0D6EFD;
+                color: #031633 !important;
+                " />
+                <label for="floatingInput" class="text-primary fw-light">Email</label>
             </div>
             <div class="form-floating">
                 <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="password"
-                    value="" />
-                <label for="floatingPassword">Password</label>
+                    value="" style="
+                background-color: #FFFFFF;
+                border-color: #0D6EFD;
+                color: #031633 !important;
+                " />
+                <label for="floatingPassword" class="text-primary fw-light">Password</label>
             </div>
 
             <button class="btn btn-primary w-100 py-2" type="submit">
@@ -52,12 +73,12 @@ session_start();
                 echo '<div class="alert alert-danger my-3" role="alert">' . $_SESSION['error'] . '</div>';
             }
             ?>
-            <p class="mt-5 mb-3 text-body-secondary">&copy; 2017–2024</p>
+            <p class="mt-5 mb-3 text-light fw-light">&copy; 2017–2024</p>
         </form>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+        </script>
 </body>
 
 </html>
